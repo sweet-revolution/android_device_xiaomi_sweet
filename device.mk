d59f7124d7a615cf44e8fcf9eadbb6146dc64761
Yo Chiang <yochiang@google.com>
2020-08-17 11:44:54 +0800

    sweet: Use on_vendor VNDK APEX
    Move VNDK APEX to /vendor partition so we don't need to update
    vendor.img prebuilt everytime there is a change in the VNDK
    libraries.
    
    Change-Id: I832191e8876306883a04c8539cf8dcda16fe940f

--- device.mk
+++ device.mk
@@ -280,6 +280,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_PACKAGES += \
+    com.android.vndk.current.on_vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
