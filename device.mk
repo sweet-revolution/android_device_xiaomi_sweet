17fd67f61c4f2f900c7cbac826aa7ed79bce51a1
Peter Cai <peter@typeblog.net>
2021-04-20 00:25:11 +0530

    sweet: parts: import XiaomiParts from android_device_xiaomi_gauguin
    https: //github.com/AdarshGrewal/android_device_xiaomi_gauguin/tree/c082d3197b84ff0fd7836d0b66f9f8ed8718a599/parts
    Co-Authored-By: Peter Cai <peter@typeblog.net>
    Co-Authored-By: Akhil Narang <me@akhilnarang.dev>
    Co-Authored-By: Adarsh Grewal <adarsh@aosip.dev>
    Co-Authored-By: bezke <1642612+bezke@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -52,6 +52,13 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd \
