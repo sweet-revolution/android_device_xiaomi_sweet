ab38289fef44ab21077836ed71590277edd83da3
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2021-10-26 22:43:16 +0700

    sweet: cherish
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- cherish_sweet.mk
+++ cherish_sweet.mk
@@ -5,21 +5,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Nusantara configurations
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
+# Inherit common Cherish configurations
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+CHERISH_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_PIXEL_CHARGING := true
-TARGET_USES_BLUR := true
+IS_PHONE := true
+WITH_GMS := true
+TARGET_INCLUDE_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 
-PRODUCT_NAME := nad_sweet
+PRODUCT_NAME := cherish_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=BayerischeMotorenWerke
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
